ee4b534df0d3994930b29718550414804f87a3a9
Angel Garcia <xengar@gmail.com>
2016-12-22 15:37:32 -0600

    Draw lines on mouse position Tutorial3

--- Tutorial03-mouseEvents.py
+++ Tutorial03-mouseEvents.py
@@ -23,8 +23,30 @@ def track_mouse_position():
         pygame.display.flip()
 
 
+# Draws lines that cut the mouse pointer’s coordinates.
+def draw_lines_using_mouse_position():
+    bgcolor = 0, 0, 0
+    linecolor = 255, 255, 255
+    x = y = 0
+    running = 1
+    screen = pygame.display.set_mode((640, 400))
+     
+    while running:
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            running = 0
+        elif event.type == pygame.MOUSEMOTION:
+            x, y = event.pos
+    
+        screen.fill(bgcolor)
+        pygame.draw.line(screen, linecolor, (x, 0), (x, 399))
+        pygame.draw.line(screen, linecolor, (0, y), (639, y))
+        pygame.display.flip()
+
+
 
 # Main section
 pygame.init()
-track_mouse_position()
+#track_mouse_position()
+draw_lines_using_mouse_position()
 pygame.quit()
